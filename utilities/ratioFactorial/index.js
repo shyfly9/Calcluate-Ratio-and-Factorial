68eec47b989183b9d4d20bcb5addb602f6b59a3c
shyam <yash.shyam007@gmail.com>
2023-01-21 09:18:35 +0000

    Calculate Ratio and Factorial

---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,9 @@
+const factorialOfNumber = require("../factorial/index");
+const ratioOfTwoNumbers = require("../ratio/index");
+
+const ratioAndFactorial = (a, b, c) => {
+  const ratio = ratioOfTwoNumbers(a, b);
+  const factorial = factorialOfNumber(c);
+  return { ratio, factorial };
+};
+module.exports = ratioAndFactorial;
